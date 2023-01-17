e0ac51ded2794a4acd0c0ba431907896d8af3ae6
chuy-zip <andreschuy03@gmail.com>
2023-01-16 22:02:49 -0600

    Created first version of UI

--- HDT1Estructuras/src/project/myRadio.java
+++ HDT1Estructuras/src/project/myRadio.java
@@ -1,9 +1,30 @@
 package project;
-
+import java.util.Scanner;
 public class myRadio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		String option;
+		
+		while(true) {
+			System.out.println("Bienvenido, la Radio actualmente se encuentra: \n"
+								+"Con el tipo de Frecuencia                    : \n"
+								+"En la Emisora			             :\n");
+			
+			System.out.println("Ingrese el numero de la opcion que desea realizar");
+		
+			System.out.println("\n1.Encender el radio\n"
+				         	+ "2.Cambiar tipo de frecuencia\n"
+				         	+ "3.Avanzar a la siguiente Emisora\n"
+				         	+ "4.Guardar emisora actual en un boton\n"
+				         	+ "5.Elegir la emisora de un boton\n"
+				         	+ "6.Apagar el radio");
+		
+			option = "aaz" + scan.nextLine();
+		
+		}
+		
 
 	}
 
